2b1046a2846fa0862daf6e520f981112cdd92148
Spychest Darkfield <spych3st@gmail.com>
2026-08-06 06:16:10 +0200

    Add profile fixture

---
+++ src/DataFixtures/ProfileFixture.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Profile;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ProfileFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $profile = new Profile();
+
+        $profile
+            ->setHeight(1.69)
+            ->setBirthDate(new \DateTimeImmutable('1991-04-08'))
+            ->setBiologicalGender('male')
+            ->setStartingWeight(150)
+            ->setTargetWeight(80);
+
+        $manager->persist($profile);
+
+        $manager->flush();
+    }
+}
