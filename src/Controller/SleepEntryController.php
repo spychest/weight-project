33a3e9a87ff75d7bd515b46d8c98e9496a86a90d
Spychest Darkfield <spych3st@gmail.com>
2026-08-14 03:09:51 +0200

    added CRUD for weight and sleep entries

--- src/Controller/SleepEntryController.php
+++ src/Controller/SleepEntryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Profile;
 use App\Entity\SleepEntry;
 use App\Form\SleepEntryType;
+use App\Repository\SleepEntryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,19 +15,27 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SleepEntryController extends AbstractController
 {
     #[Route('/sleep/new', name: 'app_sleep_new')]
+    #[Route('/sleep/new/{id}', name: 'app_sleep_edit')]
     public function new(
         Request $request,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        ?SleepEntry $sleepEntry = null
     ): Response {
-        $profile = $entityManager
-            ->getRepository(Profile::class)
-            ->findOneBy([]);
+        $editMod = true;
+        if($sleepEntry == null){
+            $editMod = false;
 
-        $sleepEntry = new SleepEntry();
+            $profile = $entityManager
+                ->getRepository(Profile::class)
+                ->findOneBy([]);
 
-        $sleepEntry
-            ->setProfile($profile)
-            ->setDate(new \DateTimeImmutable());
+            $sleepEntry = new SleepEntry();
+
+            $sleepEntry
+                ->setProfile($profile)
+                ->setDate(new \DateTimeImmutable());
+        }
+
 
         $form = $this->createForm(SleepEntryType::class, $sleepEntry);
 
@@ -35,12 +44,32 @@ final class SleepEntryController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($sleepEntry);
             $entityManager->flush();
-
-            return $this->redirectToRoute('app_dashboard');
+            if($editMod !== true) {
+                return $this->redirectToRoute('app_dashboard');
+            }
+            return $this->redirectToRoute('app_sleep_show', ['id' => $sleepEntry->getId()]);
         }
 
         return $this->render('sleep_entry/new.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/sleep', name: 'app_sleep_index')]
+    public function index(SleepEntryRepository $sleepEntryRepository): Response
+    {
+        $sleepEntries = $sleepEntryRepository->findBy([], []);
+        return $this->render('sleep_entry/index.html.twig', [
+            'controller_name' => 'SleepEntryController',
+            'sleepEntries' => $sleepEntries,
+        ]);
+    }
+
+    #[Route('/sleep/show/{id}', name: 'app_sleep_show')]
+    public function show(SleepEntry $sleepEntry): Response
+    {
+        return $this->render('sleep_entry/show.html.twig', [
+            'sleepEntry' => $sleepEntry,
         ]);
     }
 }
--- src/Controller/WeightEntryController.php
+++ src/Controller/WeightEntryController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\WeightEntry;
 use App\Form\WeightEntryType;
 use App\Repository\ProfileRepository;
+use App\Repository\WeightEntryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,18 +15,19 @@ use Symfony\Component\Routing\Attribute\Route;
 final class WeightEntryController extends AbstractController
 {
     #[Route('/weight/new', name: 'app_weight_new')]
+    #[Route('/weight/new/{id}', name: 'app_weight_edit')]
     public function new(
         Request $request,
         ProfileRepository $profileRepository,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        ?WeightEntry $weightEntry = null
     ): Response {
-        $profile = $profileRepository->findOneBy([]);
+        $editMod = true;
+        if($weightEntry == null){
+            $editMod = false;
 
-        if ($profile === null) {
-            throw new \LogicException('No profile configured.');
+            $weightEntry = new WeightEntry();
         }
-
-        $weightEntry = new WeightEntry();
 
         $form = $this->createForm(
             WeightEntryType::class,
@@ -35,16 +37,43 @@ final class WeightEntryController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $weightEntry->setProfile($profile);
+            if($editMod === false){
+                $profile = $profileRepository->findOneBy([]);
+
+                if ($profile === null) {
+                    throw new \LogicException('No profile configured.');
+                }
+                $weightEntry->setProfile($profile);
+            }
 
             $entityManager->persist($weightEntry);
             $entityManager->flush();
 
+            if($editMod !== true) {
+                return $this->redirectToRoute('app_dashboard');
+            }
             return $this->redirectToRoute('app_dashboard');
         }
 
         return $this->render('weight_entry/new.html.twig', [
             'form' => $form,
+        ]);
+    }
+
+    #[Route('/weight', name: 'app_weight_index')]
+    public function index(WeightEntryRepository $weightEntryRepository): Response
+    {
+        $weightEntries = $weightEntryRepository->findBy([], []);
+        return $this->render('weight_entry/index.html.twig', [
+            'controller_name' => 'WeightEntryController',
+            'weightEntries' => $weightEntries,
+        ]);
+    }
+
+    #[Route('/weight/show/{id}', name: 'app_weight_show')]
+    public function show(WeightEntry $weightEntry):Response{
+        return $this->render('weight_entry/show.html.twig', [
+            'weightEntry' => $weightEntry,
         ]);
     }
 }
